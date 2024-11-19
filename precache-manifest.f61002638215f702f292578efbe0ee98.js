31da945b90d4c5416087817dfd3c25efeab6195d
Nicolas <nicolas.moulin104@gmail.com>
2024-11-19 15:56:34 +0100

    Updates

---
+++ precache-manifest.f61002638215f702f292578efbe0ee98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f5fb7ca9c3739199049e01ab576803f",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "376fd0e6093e25227ac0",
+    "url": "/Portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "36757b9e423c237c8a3a",
+    "url": "/Portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "376fd0e6093e25227ac0",
+    "url": "/Portfolio/static/js/2.bef568c7.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Portfolio/static/js/2.bef568c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36757b9e423c237c8a3a",
+    "url": "/Portfolio/static/js/main.1385765a.chunk.js"
+  },
+  {
+    "revision": "c125469937b460e6de34",
+    "url": "/Portfolio/static/js/runtime-main.3379861c.js"
+  }
+]);
